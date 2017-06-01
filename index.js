334898055a2727a09cf4469874c0a88766c34a81
imkimchi <wbvcos@gmail.com>
2017-06-01 04:11:14 -0400

    add error handler

---
+++ index.js
@@ -0,0 +1,24 @@
+/**
+ * @author Philip Yoonshin <wbvcos@gmail.com
+ */
+
+/** @description better version of try/catch
+ *  @param {msg} Error Message you want to print
+ *  @example
+ *  //
+ *  import _ from util
+ *  _.error('Failed to Connect')
+ */
+
+const _ = {
+    error() {
+        let newErr = new Error(msg) 
+        return e => {
+            newErr.originalError = e
+            throw newErr
+        }
+    },
+    log = console.log
+}
+
+export default _
